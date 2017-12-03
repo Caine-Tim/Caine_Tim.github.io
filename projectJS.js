d81c9e1316cecb82ea7226312a9ca2bb1cb1ce94
Caine-Tim <31802260+Caine-Tim@users.noreply.github.com>
2017-12-03 10:45:07 -0700

    Add files via upload

--- projectJS.js
+++ projectJS.js
@@ -1,5 +1,6 @@
 //show pitt info
-function showPitt() {
+function showCrosby() {
+    'use strict';
     //show image
     document.getElementById('crosby').className ='classname';
     document.getElementById('crosby').style.display='block';
@@ -10,34 +11,35 @@ function showPitt() {
     info.onreadystatechange = function() {
         if(this.readyState == 4 && this.status == 200) {
             var name = JSON.parse(this.response);
-            var name = "Team: " + name.team + "<br>" + "Player: " + name.player;
+            var name = "Team: " + name.team + "<br>" + "Player: " + name.player + "<br>" + "Points: " + name.points + "<br>" + "Goals: " + name.goals + "<br>" + "Assits: " + name.assists;
                         
             document.getElementById("pittInfo").innerHTML = name;
         }
     }
                 
-    info.open("Get", "pitt.txt", true);
+    info.open("Get", "crosby.txt", true);
     info.send();
 }
             
-    //show chicago info
-    function showChicago() {
-        //show image
-        document.getElementById('toews').className ='classname';
-        document.getElementById('toews').style.display='block';
+//show chicago info
+function showChicago() {
+    'use strict';
+    //show image
+    document.getElementById('toews').className ='classname';
+    document.getElementById('toews').style.display='block';
                 
-        //show json
-        var info = new XMLHttpRequest();
+    //show json
+    var info = new XMLHttpRequest();
                 
-        info.onreadystatechange = function() {
-            if(this.readyState == 4 && this.status == 200) {
-                var name = JSON.parse(this.response);
-                var name = "Team: " + name.team + "<br>" + "Player: " + name.player;
+    info.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            var name = JSON.parse(this.response);
+            var name = "Team: " + name.team + "<br>" + "Player: " + name.player + "<br>" + "Points: " + name.points + "<br>" + "Goals: " + name.goals + "<br>" + "Assits: " + name.assists;
                         
-                document.getElementById("chicagoInfo").innerHTML = name;
-            }
+            document.getElementById("chicagoInfo").innerHTML = name;
         }
+    }
                 
-        info.open("Get", "chicago.txt", true);
-        info.send();
+    info.open("Get", "chicago.txt", true);
+    info.send();
 }
